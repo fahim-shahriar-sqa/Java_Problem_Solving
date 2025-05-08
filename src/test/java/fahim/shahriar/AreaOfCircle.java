c8a6210720430206ea7cb54f662f133f00a05c6c
fahim-shahriar-sqa <work.fahimshahriar@gmail.com>
2025-05-08 23:35:04 +0600

    Area of a Circle

---
+++ src/test/java/fahim/shahriar/AreaOfCircle.java
@@ -0,0 +1,15 @@
+package fahim.shahriar;
+
+import java.util.Scanner;
+
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the radius : " );
+        double r = input.nextDouble();
+        double pie = 3.14;
+        double area = pie * r * r;
+
+        System.out.println("Area of the circle is : " + area );
+    }
+}
